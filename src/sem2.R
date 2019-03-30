de88bd5cbee21df7d6f06d0d503ce4a1ded0d145
ozanarkancan <can.ozanarkan@gmail.com>
2019-03-30 15:18:29 +0300

    final

--- src/sem2.R
+++ src/sem2.R
@@ -36,6 +36,31 @@ geri_kazanma =~ aktiflestirilmis_musteri + geri_kazanilmis_musteri
 geri_kazanma ~ deneyim + elde_tutma
 "
 
+model4 <- "
+dijital_kanal =~ hata_islem_sayisi + islem_ortalama_tiklama + cevrim_ici_sure + basarili_giris_sayisi + gezdigi_sayfa_sayisi
+sosyal_medya =~ begeni + takip
+deneyim =~ memnuniyet + tavsiye + pozitif_yorum + negatif_yorum + sosyal_medya_reklamlari + gelir
+deneyim ~ dijital_kanal + sosyal_medya
+buyume =~ capraz_satis
+buyume ~ deneyim
+elde_tutma =~ devam_eden_musteri + terk
+elde_tutma ~ deneyim + buyume
+geri_kazanma =~ aktiflestirilmis_musteri + geri_kazanilmis_musteri
+geri_kazanma ~ deneyim + elde_tutma
+"
+
+fit4 <- sem(model4, gendata, std.lv = TRUE, std.ov = TRUE,
+            control = list(maxit = 100000),
+            estimator = "ULS",
+            meanstructure = TRUE,
+            optim.method = "BFGS",
+            verbose=TRUE)
+fitmeasures(fit4, c("npar", "chisq", "df", "cfi", "rmsea", "srmr"))
+
+semPaths(fit4, "std", nCharNodes = 35, layout="tree",
+         intercepts = FALSE, pastel = TRUE, residuals = FALSE, label.prop = 0.93,
+         sizeMan = 5.5, sizeLat = 8, font = 4, fade=FALSE, reorder=FALSE)
+
 fit1 <- sem(model1, gendata, std.lv = TRUE, std.ov = TRUE,
             control = list(maxit = 100000),
             estimator = "ULS",
@@ -65,7 +90,7 @@ summary(fit1, fit.measures=TRUE, standardized=TRUE, rsquare = TRUE)
 summary(fit2, fit.measures=TRUE, standardized=TRUE, rsquare = TRUE)
 summary(fit3, fit.measures=TRUE, standardized=TRUE, rsquare = TRUE)
 
-parameterestimates(fit1)
+parameterestimates(fit3)
 
 sfit <- standardizedsolution(fit1)
 
@@ -74,4 +99,7 @@ sfit[sfit$op == "=~",]
 fitmeasures(fit1)
 
 fitmeasures(fit1, c("npar", "chisq", "df", "cfi", "rmsea", "srmr"))
+fitmeasures(fit2, c("npar", "chisq", "df", "cfi", "rmsea", "srmr"))
+fitmeasures(fit3, c("npar", "chisq", "df", "cfi", "rmsea", "srmr"))
+
 
